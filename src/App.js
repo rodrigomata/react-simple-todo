0ba96a1b2a5fac0370c0d7582569630c57dbacae
royestone <rodrigo.mata.ib@gmail.com>
2021-08-04 00:06:22 -0500

    feat: finished delete fn and added complete functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { useReducer, useState } from 'react'
+import React, { useContext, useReducer, useState } from 'react'
+
+const Context = React.createContext()
 
 function appReducer(state, action) {
   switch (action.type) {
@@ -15,6 +17,14 @@ function appReducer(state, action) {
     case 'delete': {
       return state.filter(item => item.id !== action.payload.id)
     }
+    case 'complete': {
+      return state.map(item => {
+        if (item.id === action.payload.id) {
+          return { ...item, completed: !item.completed }
+        }
+        return item
+      })
+    }
     default:
       return [state]
   }
@@ -25,29 +35,33 @@ function TodoApp() {
   const [state, dispatch] = useReducer(appReducer, [])
   return (
     <main>
-      <h1 className="title">Todo list app</h1>
-      <input type="text" value={text} onChange={e => setText(e.target.value)} />
-      <button onClick={() => dispatch({ type: 'add', payload: { text } })}>Add</button>
-      <TodoList items={state} />
+      <Context.Provider value={dispatch}>
+        <h1 className="title">Todo list app</h1>
+        <input type="text" value={text} onChange={e => setText(e.target.value)} />
+        <button onClick={() => dispatch({ type: 'add', payload: { text } })}>Add</button>
+        <TodoList items={state} />
+      </Context.Provider>
     </main>
-  );
+  )
 }
 
 function TodoList({ items }) {
   return (
-  <ul className="list">
-    {items.map(item => <TodoItem key={item.id} {...item}></TodoItem>)}
-  </ul>
+    <ul className="list">
+      {items.map(item => <TodoItem key={item.id} {...item}></TodoItem>)}
+    </ul>
   )
 }
 
 function TodoItem({ id, text }) {
+  const dispatch = useContext(Context)
   return (
-  <li className="item">
-    <input type="checkbox"></input>
-    <span>{text}</span>
-    <button>Delete</button>
-  </li>)
+    <li className="item">
+      <input type="checkbox" onChange={() => dispatch({ type: 'complete', payload: { id } })} />
+      <span>{text}</span>
+      <button onClick={() => dispatch({ type: 'delete', payload: { id } })}>Delete</button>
+    </li>
+  )
 }
 
-export default TodoApp;
+export default TodoApp
